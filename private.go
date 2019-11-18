c234f96228cab723f66791c5a111f1949cc01ee0
Eric Ren <eric.ren@dapperlabs.com>
2019-11-18 10:03:23 -0800

    added inline fragment as a valid name

--- private.go
+++ private.go
@@ -8,6 +8,9 @@ import (
 // checks the validity of a name according to the spec: http://facebook.github.io/graphql/October2016/#sec-Names
 var validName = regexp.MustCompile("^[_A-Za-z][_0-9A-Za-z]*$")
 
+// check the validity of an inline fragment as a name according to the spec: https://graphql.github.io/graphql-spec/June2018/#sec-Inline-Fragments
+var validInlineFragment = regexp.MustCompile("^... on [_A-Za-z][_0-9A-Za-z]*$")
+
 func isValidOperationType(Type operationType) bool {
 	low := strings.ToLower(string(Type))
 	return low == "query" || low == "mutation" || low == "subscription"
